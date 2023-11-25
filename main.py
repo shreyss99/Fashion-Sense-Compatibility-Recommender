f2178efb03879bd4070045004b70a2d17237dd3c
Shrey Shailesh Shah <shreyss99@gmail.com>
2023-11-25 04:44:35 -0600

    Update

---
+++ main.py
@@ -0,0 +1,9 @@
+# Importing Libraries
+
+import os
+import numpy as np
+from PIL import Image
+import cv2
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
