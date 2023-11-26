f12038bffcdc3691afa34826878ce2c7f50aeeeb
Shrey Shailesh Shah <shreyss99@gmail.com>
2023-11-26 04:31:05 -0600

    Add code for extracting features from images

--- app.py
+++ app.py
@@ -17,4 +17,30 @@ resnet = tensorflow.keras.Sequential([
     GlobalMaxPooling2D()
 ])
 
-print(resnet.summary())
+# print(resnet.summary())
+
+
+# Function to extract features from an input uploaded/provided by the user
+def extract_image_features(input_path, model):
+
+    # Get the image from the input path
+    img = image.load_img(input_path, target_size=(224, 224))
+
+    # Convert the image to a numpy array
+    img_array = image.img_to_array(img)
+
+    # Reshape the image as Keras works on batch of images.
+    # Even if we have 1 image we need to show Keras that it is a batch with 1 image
+    expanded_img_array = np.expand_dims(img_array, axis=0)
+
+    # Preprocess the image to match ResNet expected format as per the ImageNet dataset
+    preprocessed_img_array = preprocess_input(expanded_img_array)
+
+    # Extract the features from the image using the ResNet model
+    # Flatten it so that it becomes a 1-D vector of size 2048
+    result = model.predict(preprocessed_img_array).flatten()
+
+    # Normalize the
+    normalized_result = result / norm(result)
+
+    return normalized_result
